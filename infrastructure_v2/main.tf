a9ac21f505f24224884a81223bd42e392cfa0872
Phil Duncan <philipduncan860@gmail.com>
2026-07-09 20:08:07 -0400

    changed terraform branch use to deploy-quay

--- infrastructure_v2/main.tf
+++ infrastructure_v2/main.tf
@@ -709,7 +709,7 @@ resource "terraform_data" "bootstrap_lab" {
       INVENTORY_FILE="${abspath(path.module)}/inventory.ini"
 
       REPO_URL="https://github.com/claudiol/image-mode-hackathon.git"
-      BRANCH="dev"
+      BRANCH="deploy-quay"
 
       echo "Using inventory: $INVENTORY_FILE"
 
