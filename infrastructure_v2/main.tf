ac4fcb83255534991e80805d9b1984c086c7697f
Phil Duncan <philipduncan860@gmail.com>
2026-07-13 14:30:46 -0400

    add temp fix for image builder quay login error

--- infrastructure_v2/main.tf
+++ infrastructure_v2/main.tf
@@ -986,7 +986,7 @@ resource "terraform_data" "bootstrap_lab" {
       INVENTORY_FILE="${abspath(path.module)}/inventory.ini"
 
       REPO_URL="https://github.com/claudiol/image-mode-hackathon.git"
-      BRANCH="tune-image-builder-server"
+      BRANCH="dev"
 
       echo "Using inventory: $INVENTORY_FILE"
 
